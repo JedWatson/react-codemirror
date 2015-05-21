99055c6b97df11f734dea9fa996cbdbd1bb835a0
Jed Watson <jed.watson@me.com>
2015-05-22 00:15:04 +1000

    Implementing Example app

--- example/src/example.js
+++ example/src/example.js
@@ -2,12 +2,25 @@ var React = require('react'),
 	Codemirror = require('react-codemirror');
 
 var App = React.createClass({
+	getInitialState: function() {
+		return {
+			code: "// Code"
+		};
+	},
+	updateCode: function(newCode) {
+		this.setState({
+			code: newCode
+		});
+	},
 	render: function() {
+		var options = {
+			lineNumbers: true
+		};
 		return (
 			<div>
-				<Codemirror />
+				<Codemirror value={this.state.code} onChange={this.updateCode} options={options} />
 			</div>
-		)
+		);
 	}
 });
 
