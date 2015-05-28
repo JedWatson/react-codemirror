69d27c9d554416ab2f72d5ff808e68add8d3b52b
Jed Watson <jed.watson@me.com>
2015-05-28 19:30:19 +1000

    Update 2015-05-28T09:30:17.055Z

--- bundle.js
+++ bundle.js
@@ -8811,7 +8811,7 @@ var CodeMirror = React.createClass({
 		return React.createElement(
 			'div',
 			{ className: className },
-			React.createElement('textarea', { ref: 'codemirror', name: this.props.path, value: this.props.value, onChange: this.valueChanged, autoComplete: 'off' })
+			React.createElement('textarea', { ref: 'codemirror', name: this.props.path, defaultValue: this.props.value, autoComplete: 'off' })
 		);
 	}
 
--- example.js
+++ example.js
@@ -1,15 +1,15 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var React = require('react'),
-    Codemirror = require('react-codemirror');
+var React = require('react');
+var Codemirror = require('react-codemirror');
 
 var App = React.createClass({
 	displayName: 'App',
 
 	getInitialState: function getInitialState() {
 		return {
-			code: '// Code'
+			code: '# Heading\n\nSome **bold** and _italic_ text\nBy [Jed Watson](https://github.com/JedWatson)'
 		};
 	},
 	updateCode: function updateCode(newCode) {
@@ -21,11 +21,7 @@ var App = React.createClass({
 		var options = {
 			lineNumbers: true
 		};
-		return React.createElement(
-			'div',
-			null,
-			React.createElement(Codemirror, { value: this.state.code, onChange: this.updateCode, options: options })
-		);
+		return React.createElement(Codemirror, { value: this.state.code, onChange: this.updateCode, options: options });
 	}
 });
 
