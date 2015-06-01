54d09563ce69a6f073a5cd2959ef43daa7c49375
Jed Watson <jed.watson@me.com>
2015-06-01 23:01:27 +1000

    Whitespace should be trimmed

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var initGulpTasks = require('react-component-gulp-tasks');
 
 /**
  * Tasks are added by the react-component-gulp-tasks package
- * 
+ *
  * See https://github.com/JedWatson/react-component-gulp-tasks
  * for documentation.
- * 
+ *
  * You can also add your own additional gulp tasks if you like.
  */
 
