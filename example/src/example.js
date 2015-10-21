90dbe67cdfa404da9a8a9ba60b9c53a640839f23
Jed Watson <jed.watson@me.com>
2015-10-21 18:48:09 +1100

    Adding mode example

--- example/src/example.js
+++ example/src/example.js
@@ -1,16 +1,33 @@
 var React = require('react');
-var Codemirror = require('react-codemirror');
+var Codemirror = require('../../src/Codemirror');
+
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/markdown/markdown');
+
+var defaults = {
+	markdown: '# Heading\n\nSome **bold** and _italic_ text\nBy [Jed Watson](https://github.com/JedWatson)',
+	javascript: 'var component = {\n\tname: "react-codemirror",\n\tauthor: "Jed Watson",\n\trepo: "https://github.com/JedWatson/react-codemirror"\n};'
+};
 
 var App = React.createClass({
 	getInitialState () {
 		return {
+			code: defaults.markdown,
 			readOnly: false,
-			code: '# Heading\n\nSome **bold** and _italic_ text\nBy [Jed Watson](https://github.com/JedWatson)'
+			mode: 'markdown',
 		};
 	},
 	updateCode (newCode) {
 		this.setState({
 			code: newCode
+		});
+	},
+	changeMode (e) {
+		var mode = e.target.value;
+		this.setState({
+			mode: mode,
+			code: defaults[mode]
 		});
 	},
 	toggleReadOnly () {
@@ -21,12 +38,17 @@ var App = React.createClass({
 	render () {
 		var options = {
 			lineNumbers: true,
-			readOnly: this.state.readOnly
+			readOnly: this.state.readOnly,
+			mode: this.state.mode
 		};
 		return (
 			<div>
 				<Codemirror ref="editor" value={this.state.code} onChange={this.updateCode} options={options} />
 				<div style={{ marginTop: 10 }}>
+					<select onChange={this.changeMode} value={this.state.mode}>
+						<option value="markdown">Markdown</option>
+						<option value="javascript">JavaScript</option>
+					</select>
 					<button onClick={this.toggleReadOnly}>Toggle read-only mode (currently {this.state.readOnly ? 'on' : 'off'})</button>
 				</div>
 			</div>
