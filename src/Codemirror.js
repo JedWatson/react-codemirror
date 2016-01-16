5ba80a4983b57c93dc174732799aba045921daba
Jed Watson <jed.watson@me.com>
2016-01-16 22:32:50 +1300

    ES6 Code Cleanup

--- src/Codemirror.js
+++ src/Codemirror.js
@@ -1,11 +1,13 @@
+let CM;
+
 if (typeof navigator !== 'undefined') {
-  var CM = require('codemirror');
+	CM = require('codemirror');
 }
 
-var React = require('react');
-var className = require('classnames');
-var CodeMirror = React.createClass({
+const React = require('react');
+const className = require('classnames');
 
+const CodeMirror = React.createClass({
 	propTypes: {
 		onChange: React.PropTypes.func,
 		onFocusChange: React.PropTypes.func,
@@ -14,14 +16,13 @@ var CodeMirror = React.createClass({
 		value: React.PropTypes.string,
 		className: React.PropTypes.any,
 	},
-
 	getInitialState () {
 		return {
-			isFocused: false
+			isFocused: false,
 		};
 	},
 	componentDidMount () {
-		var textareaNode = this.refs.textarea;
+		const textareaNode = this.refs.textarea;
 		this.codeMirror = CM.fromTextArea(textareaNode, this.props.options);
 		this.codeMirror.on('change', this.codemirrorValueChanged);
 		this.codeMirror.on('focus', this.focusChanged.bind(this, true));
@@ -35,57 +36,49 @@ var CodeMirror = React.createClass({
 			this.codeMirror.toTextArea();
 		}
 	},
-
 	componentWillReceiveProps (nextProps) {
 		if (this.codeMirror && nextProps.value !== undefined && this._currentCodemirrorValue !== nextProps.value) {
 			this.codeMirror.setValue(nextProps.value);
 		}
 		if (typeof nextProps.options === 'object') {
-			for (var optionName in nextProps.options) {
+			for (let optionName in nextProps.options) {
 				if (nextProps.options.hasOwnProperty(optionName)) {
 					this.codeMirror.setOption(optionName, nextProps.options[optionName]);
 				}
 			}
 		}
 	},
-
 	getCodeMirror () {
 		return this.codeMirror;
 	},
-
 	focus () {
 		if (this.codeMirror) {
 			this.codeMirror.focus();
 		}
 	},
-
 	focusChanged (focused) {
 		this.setState({
 			isFocused: focused
 		});
 		this.props.onFocusChange && this.props.onFocusChange(focused);
 	},
-
 	codemirrorValueChanged (doc, change) {
-		var newValue = doc.getValue();
+		const newValue = doc.getValue();
 		this._currentCodemirrorValue = newValue;
 		this.props.onChange && this.props.onChange(newValue);
 	},
-
 	render () {
-		var editorClassName = className(
+		const editorClassName = className(
 			'ReactCodeMirror',
 			this.state.isFocused ? 'ReactCodeMirror--focused' : null,
 			this.props.className
 		);
-
 		return (
 			<div className={editorClassName}>
 				<textarea ref="textarea" name={this.props.path} defaultValue={''} autoComplete="off" />
 			</div>
 		);
-	}
-
+	},
 });
 
 module.exports = CodeMirror;
