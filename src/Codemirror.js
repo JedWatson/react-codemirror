47f575171fd0f80866d5fd81e78c38b7bb529400
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2018-01-29 06:02:03 -0500

    debouncing componentWillRecieveprops causing props and nextprops to return same value

--- src/Codemirror.js
+++ src/Codemirror.js
@@ -41,7 +41,6 @@ const CodeMirror = createReactClass({
 		};
 	},
 	componentWillMount () {
-		this.componentWillReceiveProps = debounce(this.componentWillReceiveProps, 0);
 		if (this.props.path) {
 			console.error('Warning: react-codemirror: the `path` prop has been changed to `name`');
 		}
